ef29fcb82c8766afda546c9979b93061c2c1ae18
TheZerøx <77398817+damien60250@users.noreply.github.com>
2021-12-11 15:43:48 +0100

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+var element=document.getElementById('mobileNavbar');
+element.classList.add("hidden");
+
+function navbarToogleOn(){element.classList.remove("hidden");}
+function navbarToogleOff(){element.classList.add("hidden");}
+
